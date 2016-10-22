0bc7451901b6e3c2a02e095f3b5ede85af12a6a8
Stas Altsev <alstas@gmail.com>
2016-10-22 18:22:01 +0300

    UI inhencements

--- web-app/frontend/src/components/Room.js
+++ web-app/frontend/src/components/Room.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import classnames from 'classnames';
-
+import icon from '../images/wc.png';
 
 export default (props) => {
   const {thing, isBusy} = props;
   const classnames2 = classnames('room', {busy: isBusy});
 
-  return <div className={classnames2}>{thing}</div>;
+  return (
+    <div className={classnames2}>
+      <div className="title">
+        {thing}
+      </div>
+      <div className="image">
+        <img src={icon}/>
+      </div>
+    </div>
+  );
 }
 
