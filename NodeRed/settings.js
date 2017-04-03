de07c05db2258818190e590d958213786442542d
Sergey Kambalin <skambalin@klika-tech.com>
2017-04-03 09:51:30 +0300

    Use generic flow name

--- NodeRed/settings.js
+++ NodeRed/settings.js
@@ -2,10 +2,8 @@ module.exports = {
   uiPort: process.env.PORT || 1880,
   uiHost: "127.0.0.1",
   userDir: './workspace',
-
-  // The file containing the flows. If not set, it defaults to flows_<hostname>.json
-  //flowFile: 'flows.json',
-
+  flowFile: 'flows.json',
+  flowFilePretty: true,
 
   // Node-RED scans the `nodes` directory in the install directory to find nodes.
   // The following property can be used to specify an additional directory to scan.
