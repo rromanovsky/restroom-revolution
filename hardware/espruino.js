b429940b0ff52d22bc9ba75d92058c4e27f5135b
Sergey Kambalin <skambalin@klika-tech.com>
2017-04-04 20:34:05 +0300

    ESpruino js code

---
+++ hardware/espruino.js
@@ -0,0 +1,23 @@
+var wifi = require("Wifi");
+var http = require("http");
+
+var info = wifi.getIP();
+var deviceId = info.mac;
+var host = '10.42.0.1:1880';
+
+var currentState = 0;
+var prevState = 1;
+
+(function loop () {
+  console.log('Loop');
+  currentState = analogRead(D0) > 0.2 ? 1 : 0;
+
+  if (currentState != prevState) {
+    console.log('Change ' + currentState);
+    prevState = currentState;
+    http.get('http://' + host + '/change-state?deviceId=' + deviceId + '&state=' + currentState);
+  }
+
+
+  setTimeout(loop, 1000);
+})();
